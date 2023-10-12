8775db8880a54a6284dd796e3c6700c4d60263f7
HeidiFeng <heidicxf@gmail.com>
2023-10-12 21:27:07 +0800

    correct quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -31,12 +31,19 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: Vec<(String, Command)>) -> Vec<(String, Command)> {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: Vec<(String, Command)> = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
-            output.insert((string, command));
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(n) => { 
+                    let newstr = string.clone() + &"bar".repeat(n.to_owned());
+                    output.push(newstr);
+                }
+            }
         }
         output
     }
